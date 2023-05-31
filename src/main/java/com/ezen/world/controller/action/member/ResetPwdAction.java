33a766a4fad0935231cf847f46169c97f96d7f3a
jaechan625 <tommy625@naver.com>
2023-05-31 15:54:28 +0900

    dd

---
+++ src/main/java/com/ezen/world/controller/action/member/ResetPwdAction.java
@@ -0,0 +1,24 @@
+package com.ezen.world.controller.action.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.world.controller.action.Action;
+import com.ezen.world.dao.MemberDao;
+
+public class ResetPwdAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 
+		String id = request.getParameter("id");
+	    String pwd = request.getParameter("pwd");
+	    MemberDao mdao = MemberDao.getInstance();
+	    mdao.resetNewPwd(id, pwd);
+	    request.setAttribute("result", 3);
+	    request.getRequestDispatcher("member/findPwd.jsp").forward(request, response);
+	   }
+	}
